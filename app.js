cb97c71f884fcf71d82214f054482c0de74fcddd
iibor <39728771+iibor@users.noreply.github.com>
2018-10-16 14:11:06 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+//Data Module
+
+//päivitä budjetti
+var budgetController = (function() {
+  var x = 23;
+
+  var add = function(sum) {
+    return x + sum;
+  }
+
+  return {
+    publicTest: function(b) {
+      return add(b);
+    }
+  }
+
+})();
+
+// lisää uusi item taulukkoon
+
+
+//UI Module
+
+// lisää uusi item UI:hin
+
+// päivitä UI
+
+var UIController = (function() {
+
+})();
+
+// hae input valuet
+
+
+//Controller Module
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+  var z = budgetCtrl.publicTest(5);
+
+  return {
+    anotherPublic: function() {
+      console.log(z);
+    }
+  }
+
+})(budgetController, UIController);
+
+// add eventlistener ok napille
